7c7e64b85d6cdd3327fa79443992a7cc73207ccd
Marie Langlois <marie.langlois2@gmail.com>
2025-12-24 13:26:31 +0100

    p2 ok - mais manque ingress et check on replicas

--- p2/server.sh
+++ p2/server.sh
@@ -4,6 +4,10 @@ set -e
 # Installer K3s
 curl -sfL https://get.k3s.io | sh -
 
+mkdir -p /root/.kube
+cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
+chmod 600 /root/.kube/config
+
 # Exporter kubeconfig pour kubectl
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
@@ -13,10 +17,12 @@ until kubectl get nodes &>/dev/null; do
   sleep 2
 done
 
+#
+kubectl create configmap app1-index --from-file /apps/app1/index.html
+kubectl create configmap app2-index --from-file /apps/app2/index.html
+kubectl create configmap app3-index --from-file /apps/app3/index.html
+
 # Déployer les applications
-
-kubectl create configmap app1-index --from-file app1/index.html # revoir tout ça
-
 kubectl apply -f /apps/app1/deploy.yaml
 kubectl apply -f /apps/app1/service.yaml
 
