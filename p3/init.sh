b5bb7db8d6be7c7ef6e1ab6d8bf32c92b9717595
mlanglois26 <marie.langlois2@gmail.com>
2026-02-06 15:10:31 +0000

    add --server-side=true

--- p3/init.sh
+++ p3/init.sh
@@ -31,14 +31,10 @@ kubectl apply -f argocd/namespace.yaml
 
 # 5️⃣ Installer Argo CD officiel
 echo "Installation d'Argo CD..."
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
+kubectl apply -n argocd --server-side=true -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=120s
 
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
 echo ""
 
 # ajouter kubectl apply -f argocd/application.yaml
-
-
-# ajouter les apply d'ingresses, deploy et service
