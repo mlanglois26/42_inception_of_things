c4134d76a3172fa433366d3a78d904b52f7dacea
mlanglois26 <marie.langlois2@gmail.com>
2026-02-08 14:38:23 +0000

    last updates on p2

--- p2/server.sh
+++ p2/server.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -e
 
+SERVER_IP="192.168.56.110"
+
 # Installer K3s
-curl -sfL https://get.k3s.io | sh -
-
-# mkdir -p /root/.kube
-# cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
-# chmod 600 /root/.kube/config
-
-# # Exporter kubeconfig pour kubectl
-# export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+curl -sfL https://get.k3s.io | sh -s -- \
+  --node-ip "${SERVER_IP}" \
+  --advertise-address "${SERVER_IP}" \
+  --tls-san "${SERVER_IP}"
 
 # Attendre que le server K3s soit prêt
 until kubectl get nodes &>/dev/null; do
@@ -20,10 +18,8 @@ done
 # Créer les ConfigMaps avant les déploiements
 kubectl create configmap app1-index --from-file=/apps/app1/index.html --save-config -o yaml --dry-run=client | kubectl apply -f -
 kubectl create configmap app3-index --from-file=/apps/app3/index.html --save-config -o yaml --dry-run=client | kubectl apply -f -
-# enlver le create
-# faire kubectl rollout restart le deploy si on touche au fichier 
 
-# -f pour file
+
 # Déployer les pods et les services
 kubectl apply -f /apps/app1/deploy.yaml
 kubectl apply -f /apps/app1/service.yaml
